2f3df803dfe182b63f125eb83b389f0ddec3ae19
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-01-25 22:25:11 -0800

    js added

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,37 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
- * Function that is called when the document is ready.
- */
+* Function that is called when the document is ready.
+*/
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").click(function(e) {
+        $('.jumbotron h1').text("Javascript is connected");
+        $(this).text("Javascript Tested!");
+        $('.jumbotron p').toggleClass("active");
+    });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+    $(".project").click (function(e) {
+        e.preventDefault();
+        var containingProject = $(this).closest(".project");
+        var description = $(containingProject).find(".project-description");
+        if (description.length === 0) {
+            $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+        } else {
+            description = $(containingProject).find(".project-description:hidden");
+            console.log (description);
+            if (description.length === 0) {
+                description = $(containingProject).find(".project-description");
+                $(description).fadeOut();
+            }
+            else {
+                $(description).fadeIn();
+            }
+        }
+    })
 }
